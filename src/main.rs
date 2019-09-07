33f7941f82c7db144323bb0e55018615ad117315
Kalle Rennes <kalle@kallerennes.com>
2019-09-06 20:35:56 -0700

    Changed return format to Json

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,30 @@
 #![feature(proc_macro_hygiene, decl_macro)]
 
 #[macro_use] extern crate rocket;
+#[macro_use] extern crate rocket_contrib;
+use rocket_contrib::json::JsonValue;
 
 mod parser;
 use parser::parse;
+
 use std::str;
+
+
 #[get("/")]
 fn index() -> &'static str {
     ""
 }
 
 #[get("/roll/<s>")]
-fn roll(s:String) -> Option<String> {
+fn roll(s:String) -> JsonValue {
     let result = parse(s);
     match result {
-        Ok(expression) => Some(expression.to_string()),
-        Err(_) => None
+        Ok(roll_result) => json!({"result":roll_result}),
+        Err(_) => json!({})
     }
-    // if let Ok(result) = p.parse(ss.as_bytes()) {
-    //     Some(result.eval().to_string())
-    // }else {
-    //     None
-    // }
 }
 
 
 fn main() {
     rocket::ignite().mount("/", routes![index,roll]).launch();
-
-
-    // let expression = parser::dice_parser();
-    // if let Ok(e) = expression.parse(b"d10 - d10"){
-    //     println!("Expression {:?}",e);
-    //     println!("Result {:?}",e.eval());
-    // }
 }
